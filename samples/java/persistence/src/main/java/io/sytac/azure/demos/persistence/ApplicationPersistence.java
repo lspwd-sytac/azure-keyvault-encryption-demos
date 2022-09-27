6f338ee4b0f74bbca9788fe1714a56e4816b44e3
lspwd2 <aliaksei@internal.yanchuk.nl>
2022-09-27 20:05:49 +0200

    Added documentation

--- samples/java/persistence/src/main/java/io/sytac/azure/demos/persistence/ApplicationPersistence.java
+++ samples/java/persistence/src/main/java/io/sytac/azure/demos/persistence/ApplicationPersistence.java
@@ -7,10 +7,29 @@ import org.bson.types.ObjectId;
 import java.io.IOException;
 import java.util.Map;
 
-public interface ApplicationPersistence<TVal> {
-    String store(@NonNull TVal val, @NonNull ObjectIdSupplier<TVal> idSupplier) throws IOException;
+/**
+ * An over-simplified DAO-style interface encapsulating reading and writing of data to the database. The interface,
+ * in this example, chooses to directly expose BSON types.
+ * <p/>
+ * The intention of this interface is to demonstrate that database objects encryption can be fully encapsulated
+ * within an interface. So the calling code can be written without worrying about underlying encryption/decryption
+ * <p/>
+ * There are two implementations of this interface:
+ * <ol>
+ *     <li>{@link CosmosDBPersistence} providing no specific protection, and </li>
+ *     <li>{@link ProtectionDecoratingPersistence} that provides a "decoration" of runtime objects for storing these
+ *     in the database.</li>
+ * </ol>
+ * The decorator pattern is especially useful if your project is planning to use encrypting and signing.In this
+ * example, the demonstration is limited to encryption of data using one-time keys that are also stored together
+ * with the record.
+ *
+ * @param <S> concrete schema type stored in this collection
+ */
+public interface ApplicationPersistence<S> {
+    String store(@NonNull S val, @NonNull ObjectIdSupplier<S> idSupplier) throws IOException;
 
     ObjectId idOf(Document key) throws IOException;
 
-    DBIdentifiedObject<TVal> get(@NonNull Map<String, Object> reqKey) throws IOException;
+    DBIdentifiedObject<S> get(@NonNull Map<String, Object> reqKey) throws IOException;
 }
